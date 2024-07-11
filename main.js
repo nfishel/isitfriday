a291ca9cb523f7fb4d6eddcf61f84bfb7a953e7d
Fishel <nfishel@hse.k12.in.us>
2024-07-11 09:15:05 -0400

    added if

--- main.js
+++ main.js
@@ -2,5 +2,9 @@ console.log("Hello Royals!");
 
 
 const today = new Date();
-
+if today.getDate() == 5{
+    console.log(today.getDay())
+}else{
+    console.log(today.getMonth())
+}
 console.log(today);
