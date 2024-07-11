85d9daa7ced05b1300c8095b8b7056ea33ad5c8a
Fishel <nfishel@hse.k12.in.us>
2024-07-11 09:54:06 -0400

    fixed if

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ console.log("Hello Royals!");
 
 
 const today = new Date();
-if today.getDay() == 5{
+if(today.getDay() === 5){
     document.querySelector("#answer").textContent = "YES!"
 }else{
     document.querySelector("#answer").textContent = "NO :("
